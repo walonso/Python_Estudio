a564e04fc9907dacbe1511606b5ba1aeed9735c3
walonsor <walteralonso20@yahoo.com>
2019-06-04 12:45:53 -0500

    script python

--- Scripts/5_descomposicion_entrada_salida_datos.py
+++ Scripts/5_descomposicion_entrada_salida_datos.py
@@ -13,7 +13,12 @@ if len(sys.argv) == 2:
         print("Error - número es incorrecto")
         print("Ejemplo: descomposicion.py [0-9999]")
     else:
+        #Aqui va la logica
+        cadena = str(numero)
+        longitud = len(cadena)
         
+        for i in range(longitud):
+            print("{:04d}".format(int(cadena[longitud - 1- i])* 10 ** i)) 
 else:
     print("Error - Agumentos incorrectos")
     print("Ejemplo: descomposicion.py [0-9999]")
